585e0195c13f96d4b3e217a2025f3ba918a8ab6c
axel tavares <tavaresaxel95@gmail.com>
2024-05-15 15:34:32 -0400

    html and javasript completed

---
+++ assets/js/questionData.js
@@ -0,0 +1,27 @@
+var questionData = [
+    {
+        title:"Commonly used data types DO NOT INCLUDE",
+        choices:["strings","booleans", "alerts", "numbers"],
+        solution:"alerts"
+    },
+    {
+        title:"The condition in an if / else statement is enclosed withing ________",
+        choices:["quotes","curly brackets", "parentheses", "square brackets"],
+        solution:"parentheses"
+    },
+    {
+        title:"Arrays in JavaScript can be used to store",
+        choices:["numbers and strings","other arrays", "booleans", "all of the above"],
+        solution:"all of the above"
+    },
+    {
+        title:"String values must be enclosed within ______ when being assigned to variables.",
+        choices:["commas","curly brackets", "quotes", "parentheses"],
+        solution:"quotes"
+    },
+    {
+        title:"A very usedful tool used during development and debugging for printing content to the debugger is:",
+        choices:["JavaScript","termonal/bash", "for loops", "console log"],
+        solution:"console log"
+    }
+]
